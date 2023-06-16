66d45ab051f8d7805e5f61ba3cf1f4c8ef599d2e
Arswm <98147604+Arswm@users.noreply.github.com>
2023-06-17 00:39:36 +0330

    v-for training

---
+++ app.js
@@ -0,0 +1,20 @@
+app.mount('#app')
+  const app = Vue.createApp({
+    data() {
+      return {
+        data() {
+            return {
+                showBooks: true,
+              books: [
+                { title: 'Batman', author: 'bob kayn' },
+                { title: 'Spider Man', author: 'Stan Lee' },
+                { title: 'Spider Man', author: 'Stan Lee' },
+                { title: 'Superman', author: 'Wayne Borin' }
+              ]
+            }
+          }
+      }
+    }
+  })
+
+  app.mount('#app')
