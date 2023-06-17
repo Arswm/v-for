ba9c9eb27d6b84dccb360e16cd21e66c8106c66b
Arswm <98147604+Arswm@users.noreply.github.com>
2023-06-17 15:48:10 +0330

    doing some stuff

--- app.js
+++ app.js
@@ -3,13 +3,53 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       books: [
-        { title: 'Batman', author: 'bob kayn',img:'assets/batman.jpg' },
-        { title: 'Spider Man', author: 'Stan Lee', img:'assets/spiderman.jpg' },
-        { title: 'Superman', author: 'Wayne Borin', img:'assets/superman.jpg' }
+        {
+          title: "Batman",
+          author: "bob kayn",
+          img: "assets/batman.jpg",
+          isFav: false,
+        },
+        {
+          title: "Spider Man",
+          author: "Stan Lee",
+          img: "assets/spiderman.jpg",
+          isFav: false,
+        },
+        {
+          title: "Superman",
+          author: "Wayne Borin",
+          img: "assets/superman.jpg",
+          isFav: false,
+        },
+        {
+          title: "Venom",
+          author: "Stan Lee",
+          img: "assets/Venom.jpg",
+          isFav: false,
+        },
+        {
+          title: "Iron man",
+          author: "Stan Lee",
+          img: "assets/Ironman.jpg",
+          isFav: false,
+        },
+        {
+          title: "Hulk",
+          author: "Stan Lee and artist Jack Kirby",
+          img: "assets/Hulk.jpg",
+          isFav: false,
+        },
       ],
-      // url: 'https://en-gb.facebook.com/'
-    }
-  }
-})
+    };
+  },
+  methods: {
+    showBook() {
+      this.showBooks = !this.showBooks;
+    },
+    isFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
