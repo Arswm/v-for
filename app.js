33185ad8c49ccad0343b4d98b276360b19e0a29e
Arswm <98147604+Arswm@users.noreply.github.com>
2023-06-17 12:51:06 +0330

    attribiute binding

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-app.mount('#app')
-  const app = Vue.createApp({
-    data() {
-      return {
-        data() {
-            return {
-                showBooks: true,
-              books: [
-                { title: 'Batman', author: 'bob kayn' },
-                { title: 'Spider Man', author: 'Stan Lee' },
-                { title: 'Spider Man', author: 'Stan Lee' },
-                { title: 'Superman', author: 'Wayne Borin' }
-              ]
-            }
-          }
-      }
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        { title: 'Batman', author: 'bob kayn',img:'assets/batman.jpg' },
+        { title: 'Spider Man', author: 'Stan Lee', img:'assets/spiderman.jpg' },
+        { title: 'Superman', author: 'Wayne Borin', img:'assets/superman.jpg' }
+      ],
+      // url: 'https://en-gb.facebook.com/'
     }
-  })
+  }
+})
 
-  app.mount('#app')
+app.mount('#app')
